7ab68de2324cf4119d48e4c05192a44f1fdbd26d
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:02:09 +0000

    Added ?ExtensionInterface

--- EveryWorkflowPimBundle.php
+++ EveryWorkflowPimBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\PimBundle;
 
 use EveryWorkflow\PimBundle\DependencyInjection\PimExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowPimBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new PimExtension();
     }
